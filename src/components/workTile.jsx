80d74069f47a68857f7516c2bdb9cb386c99839f
Pranay Begwani <pranaybegwani@gmail.com>
2023-06-22 21:29:18 -0400

    tweaks

--- src/components/workTile.jsx
+++ src/components/workTile.jsx
@@ -11,7 +11,7 @@ export const WorkTile = (props) => {
         <div className="flex mx-auto my-auto rounded-full bg-[#ffe4e6] w-[70vw] transition duration-500 hover:scale-110 items-center" onClick={() => {
             setIsOpen(true);
         }}>
-            <div className="flex items-stretch h-[100px]">
+            <div className="flex items-stretch h-[100px] max-h-[130px]">
                 <img src={props.img} alt="bruh" className="w-[7.5vh] mx-[0.5rem] my-auto"/>
                 <p className="text-slate-700 font-semibold font-mono text-[1rem] mx-[1.25rem] my-auto w-fit ">{props.content}</p>
             </div>
